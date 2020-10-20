0101f103a430aded5836664d9cb89c2dd3d84639
Volodymyr Vyshnenko <vyshnenko2904@gmail.com>
2020-10-20 22:16:39 +0300

    Добавьте файлы проекта.

---
+++ 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static string Input(string variableName)
+        {
+            Console.Write("Enter your " + variableName + ": ");
+            return Console.ReadLine();
+
+        }
+        static void Main(string[] args)
+        {
+            string login = Input("login");
+            string firstName = Input("first name");
+            string lastName = Input("last name");
+            int age = Convert.ToInt32(Input("age"));
+            Console.Clear();
+
+            User u = new User(login, firstName, lastName, age);
+            Console.WriteLine(u.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 5/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _5
+{
+    internal class User
+    {
+        private string login;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private DateTime registerDate;
+
+        public User(string login, string name, string surname, int age)
+        {
+            this.login = login;
+            this.firstName = name;
+            this.lastName = surname;
+            this.age = age;
+            registerDate = DateTime.Now;
+        }
+        public override string ToString()
+        {
+            return "Login: " + login + " \nname: " + firstName + "\nsurname: " + lastName + "\nage: " + age + " \nDate of reistration: " + registerDate.ToString();
+        }
+    }
+}
